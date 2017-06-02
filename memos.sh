7877c10994485bc58ae8dfefd688b39d63a0bba0
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2017-06-02 14:59:47 +0100

    add vim and ros memos, to complete

--- memos.sh
+++ memos.sh
@@ -3,13 +3,21 @@
 BASE_PATH=`dirname $0`
 while [ 0 ]; do
     echo "What do you wanna see ?"
-    echo "    1. regexp"
+    echo "    1. regex"
+    echo "    2. vim"
+    echo "    3. ros"
     echo "    q. quit"
 
     printf "Enter your choice: "
     read answer
     if [ "$answer" = '1' ]; then
-        cat $BASE_PATH/assets/regexp.md
+        cat $BASE_PATH/assets/memos/regex.md
+        exit 0
+    elif [ "$answer" = '2' ]; then
+        cat $BASE_PATH/assets/memos/vim.md
+        exit 0
+    elif [ "$answer" = '3' ]; then
+        cat $BASE_PATH/assets/memos/regex.md
         exit 0
     elif [ "$answer" = 'q' ]; then
         echo "Goodbye!"
