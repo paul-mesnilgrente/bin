c5fcc435056469ea40a1317aac12d4754813692e
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2017-07-14 11:30:54 +0100

    add of one-answer functionnality in memos.sh

--- memos.sh
+++ memos.sh
@@ -22,83 +22,99 @@ function menu {
 }
 
 function check_answer_action {
-    answer="$1"
-    if [ "$answer" = 'q' ]; then
+    if [ "$action" = 'q' ]; then
         goodbye
-    elif [ ! "$answer" = 'v' -a ! "$answer" = 'e' -a ! "$answer" = 'c' ]; then
-        echo "Your answer must be 'v', 'e', 'c' or 'q'"
+    elif [ ! "$action" = 'v' -a ! "$action" = 'e' -a ! "$action" = 'c' ]; then
+        echo "Your answer must contain 'v', 'e', 'c' or 'q'"
         exit 1
     fi
 }
 
 function check_answer_number {
-    if [ "$answer" = 'q' ]; then
+    number=$1
+    if [ "$number" = 'q' ]; then
         goodbye
-    elif [ ! "$answer" -eq "$answer" ] 2>/dev/null; then
+    elif [ ! "$number" -eq "$number" ] 2>/dev/null; then
         echo "Your answer must be a positive number or 'q'"
         exit 1
     fi
     nb_file=`ls "$MEMOS_PATH" | wc -l`
-    if [ "$answer" -eq 0 -o "$answer" -gt "$nb_file" ]; then
-        echo "Your answer must be between 1 and $nb_file".
+    if [ "$number" -eq 0 -o "$number" -gt "$nb_file" ]; then
+        echo "The number must be between 1 and $nb_file".
         exit 2
     fi
 }
 
 function check_answer_title {
-    if [ "$answer" = 'q' ]; then
+    if [ "$1" = 'q' ]; then
         goodbye
     fi
 }
 
 function ask_view_memo {
-    printf "What memo do you want to view? "
-    read answer
-    check_answer_number "$answer"
-    print_memo "$answer"
+    if [ $# -eq 0 ]; then
+        printf "What memo do you want to view? "
+        read number
+    else
+        number=$1
+    fi
+    check_answer_number "$number"
+    print_memo "$number"
 }
 
 function ask_edit_memo {
-    printf "What memo do you want to edit? "
-    read answer
-    check_answer_number "$answer"
-    edit_memo "$answer"
+    if [ $# -eq 0 ]; then
+        printf "What memo do you want to edit? "
+        read number
+    else
+        number=$1
+    fi
+    check_answer_number "$number"
+    edit_memo "$number"
 }
 
 function ask_create_memo {
-    printf "What title for the memo? "
-    read answer
-    check_answer_title "$answer"
-    create_memo "$answer"
+    if [ $# -eq 0 ]; then
+        printf "What title for the memo? "
+        read filename
+    else
+        filename=$1
+    fi
+    check_answer_title "$filename"
+    create_memo "$filename"
 }
 
 function print_memo {
-    answer=$1
-    file=`ls "$MEMOS_PATH" | head -n "$answer" | tail -n 1`
+    number=$1
+    file=`ls "$MEMOS_PATH" | head -n "$number" | tail -n 1`
     cat "$MEMOS_PATH"/"$file"
 }
 
 function edit_memo {
-    answer=$1
-    file=`ls "$MEMOS_PATH" | head -n "$answer" | tail -n 1`
+    number=$1
+    file=`ls "$MEMOS_PATH" | head -n "$number" | tail -n 1`
     vim "$MEMOS_PATH"/"$file"
 }
 
 function create_memo {
-    answer=$1
-    vim "$MEMOS_PATH"/"$answer"
+    filename=$1
+    vim "$MEMOS_PATH"/"$filename"
 }
 
 menu
 read answer
-check_answer_action "$answer"
-if [ "$answer" = 'v' ]; then
-    ask_view_memo
-elif [ "$answer" = 'e' ]; then
-    ask_edit_memo
-    goodbye
-elif [ "$answer" = 'c' ]; then
-    ask_create_memo
+action=`echo "$answer" | cut -d ' ' -f 1 | sed 's/[0-9]//g'`
+opt_number=`echo "$answer" | cut -d ' ' -f 1 | sed 's/[a-Z]//g'`
+check_answer_action "$action"
+
+if [ "$action" = 'v' ]; then
+    ask_view_memo $opt_number
+elif [ "$action" = 'e' ]; then
+    ask_edit_memo $opt_number
+    goodbye $opt_number
+elif [ "$action" = 'c' ]; then
+    opt_filename=`echo "$answer" | cut -s -d ' ' -f 2`
+    ask_create_memo $opt_filename
     goodbye
 fi
 
