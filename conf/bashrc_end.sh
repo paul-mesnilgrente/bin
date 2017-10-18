b0ef36e34107db17a2872b90fc3cbf3e44ba43f9
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2017-10-18 12:31:46 +0100

    add of the welcome message in the bashrc_end

--- conf/bashrc_end.sh
+++ conf/bashrc_end.sh
@@ -2,3 +2,5 @@
 if [ -f /usr/local/lib/python2.7/dist-packages/powerline/bindings/bash/powerline.sh ]; then
     source /usr/local/lib/python2.7/dist-packages/powerline/bindings/bash/powerline.sh
 fi
+
+welcome_message.sh
