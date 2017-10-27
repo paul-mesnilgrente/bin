72d8767dc8c7fba93c36c3f474673cd4d1aa584d
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2017-10-27 14:34:52 +0100

    add of tab_title function

--- conf/bashrc_end.sh
+++ conf/bashrc_end.sh
@@ -3,4 +3,14 @@ if [ -f /usr/local/lib/python2.7/dist-packages/powerline/bindings/bash/powerline
     source /usr/local/lib/python2.7/dist-packages/powerline/bindings/bash/powerline.sh
 fi
 
+function tab_title {
+  if [ -z "$1" ]
+  then
+    title=${PWD##*/} # current directory
+  else
+    title=$1 # first param
+  fi
+  echo -n -e "\033]0;$title\007"
+}
+
 $HOME/bin/welcome_message.sh
