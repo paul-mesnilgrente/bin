1c1b3776a0a9fe11d27f2a91e5476c3d5627996e
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2018-06-29 10:38:17 +0200

    add the exports in bashrc_end with checks to avoid adding them twice

--- conf/bashrc_end.sh
+++ conf/bashrc_end.sh
@@ -1,4 +1,4 @@
-# POwERLINE
+# POWERLINE
 if [ "$TERM" != "linux" ]; then
     if type "powerline" &> /dev/null; then
         usr_prefix='/usr/local/lib'
@@ -6,6 +6,24 @@ if [ "$TERM" != "linux" ]; then
         if [ -f "$usr_prefix/python3.6/$usr_suffix" ]; then
             source "$usr_prefix/python3.6/$usr_suffix"
         fi
+    fi
+fi
+
+# use user global npm instead of the system one
+echo $PATH | grep '.npm-global' > /dev/null
+if [ $? -ne 0 ]; then
+    export PATH="$HOME/.npm-global/bin:$PATH"
+fi
+
+# pyenv configuration
+if [ ! -d "${HOME}/.pyenv/bin" ]; then
+    echo "You should install pyenv."
+else
+    echo $PATH | grep '.pyenv' > /dev/null
+    if [ $? -ne 0 ]; then
+        export PATH="~/.pyenv/bin:$PATH"
+        eval "$(pyenv init -)"
+        eval "$(pyenv virtualenv-init -)"
     fi
 fi
 
