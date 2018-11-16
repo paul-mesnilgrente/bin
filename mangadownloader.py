8d494f68379281020c94fca0dbc227b5b1bb199a
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2018-11-16 19:19:58 +1300

    mangadownloader improvement, input selection is now possible

--- mangadownloader.py
+++ mangadownloader.py
@@ -5,30 +5,29 @@ from urllib.parse import urlparse, urljoin
 from pathlib import Path
 from os.path import splitext, isdir
 from os import mkdir
+from collections import OrderedDict
+
+import mimetypes
 import requests
 import shutil
 import argparse
 
 
-def get_args():
+def get_args(sources):
     parser = argparse.ArgumentParser(description='Download pictures from a \
                                      scan on lelscan')
-    parser.add_argument('-i', '--interactive',
-                        type=bool,
-                        required=False,
-                        help='choose this option to select from choices')
     parser.add_argument('-s', '--source',
-                        type=str,
-                        required=True,
+                        choices=sources,
                         help='the source to download the manga from')
     parser.add_argument('-m', '--manga',
                         type=str,
-                        required=True,
                         help='the manga to download')
-    parser.add_argument('-c', '--chapter',
-                        type=int,
-                        required=True,
+    parser.add_argument('-c', '--chapters',
+                        type=str,
                         help='the chapters to download')
+    parser.add_argument('-o', '--overwrite',
+                        type=bool,
+                        help='overwrite the folder')
     return parser.parse_args()
 
 
@@ -43,7 +42,12 @@ class Image:
         if self.response.status_code != 200:
             print('ERROR: downloading image {}.'.format(image))
 
-    def save(self, path):
+    def save(self, folder):
+        mimetypes.init()
+        mimetype = self.response.headers['content-type'].split()[0].rstrip(";")
+        ext = mimetypes.guess_extension(mimetype)
+        ext = '.jpg' if ext == '.jpe' else ext
+        path = '{}/{:02d}{}'.format(folder, self.number, ext)
         with open(path, 'wb') as f:
             self.response.raw.decode_content = True
             shutil.copyfileobj(self.response.raw, f)
@@ -74,7 +78,7 @@ class Manga:
     def __init__(self, name, url):
         self.name = name
         self.url = url
-        self.chapters = {}
+        self.chapters = OrderedDict()
 
     def __eq__(self, other):
         if isinstance(other, str):
@@ -86,7 +90,7 @@ class Manga:
 class Source:
     def __init__(self, url):
         self.url = urlparse(url)
-        self.mangas = {}
+        self.mangas = OrderedDict()
 
     def base_url(self):
         return self.url.scheme + '://' + self.url.netloc
@@ -106,8 +110,7 @@ class Source:
         mkdir(folder)
         for page in chapter.pages:
             src = page.image.url
-            path = '{}/{:02d}{}'.format(folder, page.number, splitext(urlparse(src).path)[1])
-            page.image.save(path)
+            page.image.save(folder)
 
 
 class Lelscan(Source):
@@ -127,7 +130,10 @@ class Lelscan(Source):
         options = d('form[action="/lecture-en-ligne.php"] select:nth-child(1) option')
         for option in options.items():
             url = option.attr.value
-            chapter_number = int(option.text())
+            try:
+                chapter_number = int(option.text())
+            except:
+                chapter_number = float(option.text())
             manga.chapters[chapter_number] = Chapter(chapter_number, url)
 
     def load_chapter(self, chapter):
@@ -200,31 +206,117 @@ class MangaReader(Source):
         raise NotImplementedError('please implement this function')
 
 
+class Input:
+    @staticmethod
+    def select_from_dict(message, dic, object_str):
+        obj = dic.get(object_str)
+        while obj is None:
+            print(message)
+            keys = []
+            for i, key in enumerate(dic.keys()):
+                keys.append(key)
+                print('    ({}) {}'.format(i + 1, key))
+            answer = int(input('Please input your choice: '))
+            if answer < 1 or answer > len(keys):
+                obj = None
+                print('Wrong input.')
+            else:
+                obj = dic[keys[answer - 1]]
+        return obj
+        
+    @staticmethod
+    def select_source(sources, source_str):
+        return Input.select_from_dict('Available sources: ', sources, source_str)
+
+    @staticmethod
+    def select_manga(mangas, manga_str):
+        return Input.select_from_dict('Available mangas: ', mangas, manga_str)
+
+    @staticmethod
+    def convert_ranges(ranges):
+        chapters_str = chapters_str.lower().replace(' ', '')
+        lists_str = chapters_str.split(',')
+        res = set()
+        for item in lists_str:
+            if item.contains('-'):
+                begin = int(item.split('-')[0])
+                end = int(item.split('-')[1])
+                [res.add(i) for i in range(begin, end)]
+            else:
+                try:
+                    res.add(int(item))
+                except:
+                    res.add(float(item))
+        return res
+        
+
+    @staticmethod
+    def convert_chapters_arg(chapters, chapters_str):
+        """
+        Convert into a set of float and int corresponding
+        to chapter numbers
+        """
+        res = set()
+        # parameter not given
+        if chapters_str is None:
+            res = None
+        # every chapters
+        elif chapters_str.lower() == 'all':
+            res = res.union(chapters.keys())
+        # last chapter
+        elif chapters_str.lower() == 'last':
+            res.add(max(chapters.keys()))
+        # given chapters
+        else:
+            chapters_inputed = Input.convert_ranges(chapters_str)
+            for chapter in chapters_inputed:
+                if chapter not in chapters:
+                    print('ERROR:', chapter, 'not in the list.')
+                    chapters_inputed = None
+            res = None if chapters_inputed is None else res.union(chapters_inputed)
+            
+        return res
+
+    @staticmethod
+    def select_chapters(chapters, chapters_str):
+        res = None
+        while res is None:
+            for key in chapters:
+                print('    ({}) {}'.format(key, chapters[key]))
+            if chapters_str is None:
+                chapters_str = input('Please input your choice (all/last/[0-9]*,[0-9]*...): ')
+            print('You inputed: ', chapters_str)
+            res = Input.convert_chapters_arg(chapters, chapters_str)
+        return res
+
+
 if __name__ == '__main__':
-    args = get_args()
     sources = {
         'lelscan': Lelscan(),
         'scantrad': Scantrad(),
         'mangareader': MangaReader()
     }
-    source = sources[args.source]
+    args = get_args(sources.keys())
+    source = Input.select_source(sources, args.source)
     # load mangas
-    print('Loading mangas from {}...'.format(args.source))
+    print('Loading manga list...')
     source.load_mangas()
 
     # load manga chapters
     print('Loading "{}" chapters...'.format(args.manga))
-    manga = source.mangas[args.manga]
+    manga = Input.select_manga(source.mangas, args.manga)
     source.load_manga_chapters(manga)
 
     # load chapter
-    print('Loading chapter {} of "{}"...'.format(args.chapter, args.manga))
-    chapter = manga.chapters[args.chapter]
-    source.load_chapter(chapter)
-
-    # save chapter
-    print('Saving "{}" - {:03d}...'.format(args.manga, args.chapter))
-    folder = '{} - {:03d}'.format(manga.name, args.chapter).replace(' ', '_')
-    if isdir(folder):
-        shutil.rmtree(folder)
-    source.save_chapter(chapter, folder)
+    print('Loading chapters...')
+    chapters_number = Input.select_chapters(manga.chapters, args.chapters)
+    for number in chapters_number:
+        chapter = manga.chapters[number]
+        print('Downloading chapter', chapter.number, 'of "{}"'.format(manga.name))
+        source.load_chapter(chapter)
+        # save chapter
+        print('Saving chapter', chapter.number, 'of "{}"'.format(manga.name))
+        folder = '{} - {:03d}'.format(manga.name, chapter.number).replace(' ', '_')
+        if args.overwrite and isdir(folder):
+            shutil.rmtree(folder)
+        source.save_chapter(chapter, folder)
