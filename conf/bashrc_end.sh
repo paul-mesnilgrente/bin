a565d5112e22477e28d9bdcea62c8908eb6fce9f
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2019-03-18 18:32:17 +1300

    fix bugs on bashrc_end

--- conf/bashrc_end.sh
+++ conf/bashrc_end.sh
@@ -9,21 +9,32 @@ if [ "$TERM" != "linux" ]; then
     fi
 fi
 
+source "${HOME}"/bin/tab_title.sh
+source "${HOME}"/bin/custom_output.sh
+"${HOME}"/bin/welcome_message.sh
+
 # use user global npm instead of the system one
 echo $PATH | grep '.npm-global' > /dev/null
 if [ $? -ne 0 ]; then
     export PATH="$HOME/.npm-global/bin:$PATH"
 fi
 
-# pyenv configuration
-if [ ! -d "${HOME}/.pyenv/bin" ]; then
-    echo "You should install pyenv."
+# use user global npm instead of the system one
+if [ -d "${HOME}/.rbenv/bin" ]; then
+    echo $PATH | grep '.rbenv' > /dev/null
+    if [ $? -ne 0 ]; then
+        export PATH="$HOME/.rbenv/bin:$PATH"
+    fi
+    eval "$(rbenv init -)"
 else
+    echo "You should install rbenv."
+fi
+
+# pyenv configuration
+if [ -d "${HOME}/.pyenv/bin" ]; then
     export PATH="${HOME}/.pyenv/bin:$PATH"
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
+else
+    echo "You should install pyenv."
 fi
-
-source "${HOME}"/bin/tab_title.sh
-source "${HOME}"/bin/custom_output.sh
-"${HOME}"/bin/welcome_message.sh
