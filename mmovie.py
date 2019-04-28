da15fa8bb538dcec28c31f5a21b1c0bc73c847cf
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2019-04-28 22:41:23 +1200

    add ssh command to download a movie from web transmission

--- mmovie.py
+++ mmovie.py
@@ -1,50 +1,26 @@
+#!/usr/bin/env python
+
 from argparse import ArgumentParser
-from tmdb3 import set_key
-from tmdb3 import set_locale
-from tmdb3 import searchMovie, searchMovieWithYear
-
-from pathlib import Path
-import re
-
-
-def get_key():
-    path = Path(Path.home() + '.tmdb.key')
-    if path.exists():
-        return path.read_text()
-    key = input('Please enter the api key:')
-    path.write_text(key)
-    # return 'aa898267ad6c91cf89ae0c2afaf167c2'
-    return 'aa898267ad6c91cf89ae0c2afaf167c2'
-
-
-def set_current_locale(args):
-    language = args.locale.split('-')[0]
-    country = args.locale.split('-')[1]
-    set_locale(language.lower(), country.lower())
-
-
-def find_movie(args):
-    set_key(get_key())
-    set_current_locale()
-
-    # if date given
-    match = re.match(r'.* (...)$', args.movie)
-    if match:
-        search_f = searchMovieWithYear()
-    else:
-        search_f = searchMovie
-    for i, movie in enumerate(search_f(args.movie)):
-        print('Which movie is the good one?')
-        print('   ', i, ') Original title:', movie.originaltitle)
-        print('French title:', movie.title)
-
+import os
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Move a movie file to the good folder')
-    parser.add_argument('-m', '--movie', type=str, required=True)
-    parser.add_argument('-f', '--file', type=int, required=True)
-    parser.add_argument('-d', '--directory', type=str,
-                        default='$HOME/Videos/Movies/')
-    parser.add_argument('-l', '--locale', type=str,
-                        default='fr-FR')
+    parser = ArgumentParser(description='Download a file')
+    parser.add_argument('-u', '--url',   type=str, required=True)
+    parser.add_argument('-t', '--title', type=str, required=True)
+    parser.add_argument('-y', '--year',  type=int, required=True)
     args = parser.parse_args()
+
+    source_command = 'source "$HOME/.transmission.auth"'
+    folder = '/home/Videos/Movies/{}_({})'.format(
+        args.title.replace(' ', '_'), args.year)
+    mkdir = 'mkdir "{}"'.format(folder)
+    chown = 'chown debian-transmission:debian-transmission "{}"'.format(folder)
+    transmission_command = 'transmission-remote -ne -a {} -w "{}"'.format(
+        args.url, folder)
+    command = '{} && {} && {} && {}'.format(
+        source_command,
+        mkdir,
+        chown,
+        transmission_command)
+    print(command)
+    os.system("ssh paul-mesnilgrente.com '{}'".format(command))
