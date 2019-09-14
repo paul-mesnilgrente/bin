1a22ca461f108ab8a9fb5312f381bc1ceee96604
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2019-09-14 15:26:32 +1200

    add the possibility to download a torrent from a torrent file
    some websites have protected links to torrent, meaning that the torrent
    has to be downloaded locally before to be sent to transmission

--- mmovie.py
+++ mmovie.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
+from argparse import FileType
 import os
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Download a file')
-    parser.add_argument('-u', '--url',   type=str, required=True)
-    parser.add_argument('-t', '--title', type=str, required=True)
-    parser.add_argument('-y', '--year',  type=int, required=True)
+    parser.add_argument('--torrent',     type=FileType('r'), required=False)
+    parser.add_argument('-u', '--url',   type=str,           required=False)
+    parser.add_argument('-t', '--title', type=str,           required=True)
+    parser.add_argument('-y', '--year',  type=int,           required=True)
     args = parser.parse_args()
+
+    if (args.url is None and args.torrent is None) or (args.url is not None and args.torrent is not None):
+        raise Error('Please provide a torrent file OR a url')
+
+    if args.torrent is not None:
+        os.system("scp '{}' paul-mesnilgrente.com:/tmp/my.torrent".format(args.torrent.name))
+        args.torrent = '/tmp/my.torrent'
 
     source_command = 'source "$HOME/.transmission.auth"'
     folder = '/home/Videos/Movies/{}_({})'.format(
@@ -16,7 +25,7 @@ if __name__ == '__main__':
     mkdir = 'mkdir "{}"'.format(folder)
     chown = 'chown debian-transmission:debian-transmission "{}"'.format(folder)
     transmission_command = 'transmission-remote -ne -a "{}" -w "{}"'.format(
-        args.url, folder)
+        args.url or args.torrent, folder)
     command = '{} && {} && {} && {}'.format(
         source_command,
         mkdir,
